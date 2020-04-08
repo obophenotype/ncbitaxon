70c80cd5e18c865c6fde9099dc8fcb3a9ab9b4c9
James A. Overton <james@overton.ca>
2020-04-08 12:27:43 -0400

    Compare selected taxa to previous implementation

--- Makefile
+++ Makefile
@@ -5,7 +5,13 @@ all: ncbitaxon.owl ncbitaxon.obo
 clean:
 	rm -rf build
 
-ROBOT := java -Xmx16g -jar bin/robot.jar
+build:
+	mkdir -p $@
+
+ROBOT := java -Xmx16g -jar build/robot.jar
+build/robot.jar: | build
+	curl -L -o $@ https://build.obolibrary.io/job/ontodev/job/robot/job/mireot-rdfxml/lastSuccessfulBuild/artifact/bin/robot.jar
+# curl -L -o $@ https://github.com/ontodev/robot/releases/download/v1.6.0/robot.jar
 
 build/taxdmp.zip: | build
 	curl -L -o $@ https://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
@@ -15,5 +21,52 @@ ncbitaxon.ttl: src/ncbitaxon.py src/prologue.ttl build/taxdmp.zip
 
 .PRECIOUS: ncbitaxon.owl
 .PRECIOUS: ncbitaxon.obo
-ncbitaxon.owl ncbitaxon.obo: ncbitaxon.ttl
+ncbitaxon.owl ncbitaxon.obo: ncbitaxon.ttl | build/robot.jar
 	$(ROBOT) convert -i $< -o $@
+
+
+
+
+
+
+### Build Nov2019 version for comparison
+
+build/Nov2019:
+	mkdir -p $@
+
+build/Nov2019/taxonomy_original.dat: | build/Nov2019
+	curl -L -o $@ http://ftp.ebi.ac.uk/pub/databases/taxonomy/taxonomy.dat
+
+build/Nov2019/taxonomy.dat: build/Nov2019/taxonomy_original.dat
+	sed /^MISSPELLING/d $< > $@
+
+build/Nov2019/taxdmp.zip: | build/Nov2019
+	curl -L -o $@ https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_archive/taxdmp_2019-11-01.zip
+
+build/Nov2019/ncbi2owl: bin/ncbi2owl | build/Nov2019
+	cp $< $@
+
+build/Nov2019/ncbi2owl.jar: bin/ncbi2owl.jar | build/Nov2019
+	cp $< $@
+
+build/Nov2019/ncbitaxon.owl: build/Nov2019/ncbi2owl build/Nov2019/ncbi2owl.jar build/Nov2019/taxonomy.dat build/Nov2019/taxdmp.zip
+	cd build/Nov2019 && NCBI_MEMORY=16G ./ncbi2owl -t
+
+build/Nov2019/ncbitaxon_small.owl: src/extract_test.py build/Nov2019/ncbitaxon.owl taxa.txt
+	python3 $^ $@
+
+build/Nov2019/ncbitaxon_new.ttl: src/ncbitaxon.py src/prologue.ttl build/Nov2019/taxdmp.zip taxa.txt
+	python3 $^ $@
+
+build/Nov2019/ncbitaxon_new.owl: build/Nov2019/ncbitaxon_new.ttl
+	$(ROBOT) convert -i $< -o $@
+
+build/Nov2019/ncbitaxon_new_small.owl: src/extract_test.py build/Nov2019/ncbitaxon_new.owl taxa.txt
+	python3 $^ $@
+
+.PHONY:
+build/Nov2019/ncbitaxon.diff: build/Nov2019/ncbitaxon_small.owl build/Nov2019/ncbitaxon_new_small.owl
+	diff $^ > $@
+# $(ROBOT) diff --left $(word 1,$^) --right $(word 2,$^) --output $@
+
+test: build/Nov2019/ncbitaxon.diff
--- src/ncbitaxon.py
+++ src/ncbitaxon.py
@@ -29,38 +29,73 @@ predicates = {
   'teleomorph'          : related_synonym
 }
 
-
+nodes_fields = [
+        "tax_id",                           # node id in GenBank taxonomy database
+        "parent_tax_id",                    # parent node id in GenBank taxonomy database
+        "rank",                             # rank of this node (superkingdom, kingdom, ...) 
+        "embl_code",                        # locus-name prefix; not unique
+        "division_id",                      # see division.dmp file
+        "inherited_div_flag",               # (1 or 0) 1 if node inherits division from parent
+        "genetic_code_id",                  # see gencode.dmp file
+        "inherited_GC_flag",                # (1 or 0) 1 if node inherits genetic code from parent
+        "mitochondrial_genetic_code_id",    # see gencode.dmp file
+        "inherited_MGC_flag",               # (1 or 0) 1 if node inherits mitochondrial gencode from parent
+        "GenBank_hidden_flag",              # (1 or 0) 1 if name is suppressed in GenBank entry lineage
+        "hidden_subtree_root_flag",         # (1 or 0) 1 if this subtree has no sequence data yet
+        "comments"                          # free-text comments and citations
+]
 
 def convert_names(tax_id, names):
     output = []
     for name, unique, name_class in names:
-        if name_class in predicates:
-            predicate = predicates[name_class]
+        if name_class == "scientific name":
             synonym = name
             if unique != '':
                 synonym = unique
             synonym = synonym.replace('"', '\\"')
             output.append(f"""
-NCBITaxon:{tax_id} {predicate} "{synonym}" .
+NCBITaxon:{tax_id} rdfs:label "{synonym}"^^xsd:string .""")
+        elif name_class in predicates:
+            synonym = name.replace('"', '\\"')
+            predicate = predicates[name_class]
+            synonym_type = name_class.replace(" ", "_")
+            output.append(f"""
+NCBITaxon:{tax_id} {predicate} "{synonym}"^^xsd:string .
 [ a owl:Axiom
 ; owl:annotatedSource NCBITaxon:{tax_id}
 ; owl:annotatedProperty {predicate}
-; owl:annotatedTarget "{synonym}"
-; oboInOwl:hasSynonymType "{name_class}"
+; owl:annotatedTarget "{synonym}"^^xsd:string
+; oboInOwl:hasSynonymType ncbitaxon:{synonym_type}
 ] .""")
     return output
 
 
-def convert_node(tax_id, parent_tax_id, rank, names):
+def convert_node(node, merged, names):
+    tax_id = node["tax_id"]
     output = [f"NCBITaxon:{tax_id} a owl:Class"]
+
+    parent_tax_id = node["parent_tax_id"]
     if parent_tax_id and parent_tax_id != "" and parent_tax_id != tax_id:
-        output.append(f"; rdfs:subClassOf NCBITaxon:{tax_id}")
-    if rank and rank != "":
+        output.append(f"; rdfs:subClassOf NCBITaxon:{parent_tax_id}")
+
+    # TODO: cohort, subcohort
+    rank = node["rank"]
+    if rank and rank != "" and rank != "no rank":
         rank = rank.replace(" ", "_")
         output.append(f"; ncbitaxon:has_rank NCBITaxon:{rank}")
+    
+    for merge in merged:
+        output.append(f'; oboInOwl:hasAlternativeId "NCBITaxon:{merge}"^^xsd:string')
+
+    gc_id = node["genetic_code_id"]
+    if gc_id:
+        output.append(f'; oboInOwl:hasDbXref "GC_ID:{gc_id}"^^xsd:string')
+
     output.append('; oboInOwl:hasOBONamespace "ncbi_taxonomy"^^xsd:string')
     output.append(".")
+
     output += convert_names(tax_id, names)
+
     return "\n".join(output)
 
 
@@ -73,13 +108,27 @@ def main():
     parser.add_argument("taxdmp",
             type=str,
             help="The taxdmp.zip file to read")
+    parser.add_argument("taxa",
+            type=str,
+            nargs="?",
+            help="A list of taxa to build")
     parser.add_argument("turtle",
             type=str,
             help="The output Turtle file to write")
     args = parser.parse_args()
 
-    limit = 1e7 # highest tax_id is currently 2,725,057
+    limit = None
+    #limit = 1e7 # highest tax_id is currently 2,725,057
+
+    taxa = None
+    if args.taxa:
+        taxa = set()
+        with open(args.taxa) as taxalist:
+            for line in taxalist:
+                taxa.add(line.strip())
+
     names = defaultdict(list)
+    merged = defaultdict(list)
     with open(args.turtle, "w") as turtle:
         with open(args.prologue) as prologue:
             turtle.write(prologue.read())
@@ -88,16 +137,30 @@ def main():
             with taxdmp.open("names.dmp") as dmp:
                 for line in io.TextIOWrapper(dmp):
                     tax_id, name, unique, name_class, _ = [x.strip() for x in line.split("|")]
-                    names[tax_id].append([name, unique, name_class])
-                    if int(tax_id) > limit:
+                    if taxa and tax_id in taxa:
+                        names[tax_id].append([name, unique, name_class])
+                    if limit and int(tax_id) > limit:
                         break
+
+            with taxdmp.open("merged.dmp") as dmp:
+                for line in io.TextIOWrapper(dmp):
+                    old_tax_id, new_tax_id, _ = [x.strip() for x in line.split("|")]
+                    if taxa and new_tax_id in taxa:
+                        merged[new_tax_id].append(old_tax_id)
+
             # TODO: citations
+
             with taxdmp.open("nodes.dmp") as dmp:
                 for line in io.TextIOWrapper(dmp):
-                    tax_id, parent_tax_id, rank, _ = [x.strip() for x in line.split("|", 3)]
-                    result = convert_node(tax_id, parent_tax_id, rank, names[tax_id])
-                    turtle.write(result)
-                    if int(tax_id) > limit:
+                    node = {}
+                    fields = [x.strip() for x in line.split("|")]
+                    for i in range(0, min(len(fields), len(nodes_fields))):
+                        node[nodes_fields[i]] = fields[i]
+                    tax_id = node["tax_id"]
+                    if taxa and tax_id in taxa:
+                        result = convert_node(node, merged[tax_id], names[tax_id])
+                        turtle.write(result)
+                    if limit and int(tax_id) > limit:
                         break
             # TODO: merged
             # TODO: delnodes
